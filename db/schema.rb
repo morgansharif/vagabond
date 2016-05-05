38b26def3628a745718595ea7afe47f1b5382c21
seanvsville <seanvanho@gmail.com>
2016-05-05 13:17:33 -0700

    ran migrate against start-end date col removal from iten

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505195518) do
+ActiveRecord::Schema.define(version: 20160505201343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,6 @@ ActiveRecord::Schema.define(version: 20160505195518) do
   create_table "itineraries", force: :cascade do |t|
     t.string   "name"
     t.string   "desc"
-    t.string   "start_date"
-    t.string   "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "city_id"
