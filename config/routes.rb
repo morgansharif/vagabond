d7da17490a6dfd1937c06984acd5b095fd4e4791
Ameer Brown <akeithbrown@gmail.com>
2016-05-05 09:51:19 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,54 @@ Rails.application.routes.draw do
 
   root 'site#index'
 
-  # User Routes
-  #METHOD   #PATH                 #TO     #CONTROLLER#ACTION    #ALIAS
-  get       'site/index'          =>      'site#index',         as: :index
+  # city routes
+  get '/cities', to: 'cities#index', as: :cities
+  post '/cities', to: 'cities#create'
+  get '/cities/new', to: 'cities#new', as: :new_city
+  get '/cities/:id/edit', to: 'cities#edit', as: :edit_city
+  get '/cities/:id', to: 'cities#show', as: :city
+  patch '/cities/:id', to: 'cities#update'
+  put '/cities/:id', to: 'cities#update'
+  delete '/cities/:id', to: 'cities#destroy'
+
+ # city_itinerary routes
+ get    '/cities/:id/itineraries', to: 'itineraries#index'  , as: :city_itineraries
+ post   '/cities/:id/itineraries', to:  'itineraries#create'
+ get    '/cities/:id/itineraries/new', to: 'itineraries#new', as: :new_city_itinerary
+ get    '/cities/:id/itineraries/:id/edit', to: 'itineraries#edit', as: :edit_city_itinerary
+ get    '/cities/:id/itineraries/:id', to:'itineraries#show', as: :city_itinerary
+ patch  '/cities/:id/itineraries/:id', to: 'itineraries#update'
+ put    '/cities/:id/itineraries/:id', to:  'itineraries#update'
+ delete '/cities/:id/itineraries/:id',  to: 'itineraries#destroy'
+
+# user_itinerary routes
+get    '/users/:id/itineraries', to: 'itineraries#index'  , as:   :user_itineraries
+post   '/users/:id/itineraries', to:  'itineraries#create'
+get    '/users/:id/itineraries/new', to: 'itineraries#new', as: :new_user_itinerary
+get    '/users/:id/itineraries/:id/edit', to: 'itineraries#edit', as: :edit_user_itinerary
+get    '/users/:id/itineraries/:id', to:'itineraries#show', as: :user_itinerary
+patch  '/users/:id/itineraries/:id', to: 'itineraries#update'
+put    '/users/:id/itineraries/:id', to:  'itineraries#update'
+delete '/users/:id/itineraries/:id',  to: 'itineraries#destroy'
+
+# sessions routes
+get "/login", to: "sessions#new", as: :login
+post "/sessions", to: "sessions#create"
+get  "/logout", to: "sessions#destroy"
+
+
+
+  resources :users do
+    resources :itineraries do
+      resources :activities
+  end
+ end
+
+
+resources :cities do
+  resources :itineraries do
+end
+end
 
 
 end
