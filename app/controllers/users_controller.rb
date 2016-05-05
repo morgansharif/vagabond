3f907c28d79157b427821038c4844c30890301eb
lamchristine <christine.lam.06@gmail.com>
2016-05-04 19:35:27 -0700

    added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/delete'
+
+  get 'users/new'
+
+  get 'users/edit'
+
   root 'site#index'
 
   # User Routes
