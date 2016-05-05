b70c5d8a82b553c9e3431c97140482c5a9711548
Ameer Brown <akeithbrown@gmail.com>
2016-05-05 10:49:43 -0700

    last commit and add. , ALL routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,33 @@
 Rails.application.routes.draw do
 
-  root 'site#index'
+  # root route
+  root 'site#index', as: :index
 
+  # user routes
   get  'users/new', to: 'users#new', as: :new_user
   post 'users', to: 'users#create'
   get  'users/:id/edit', to: 'users#edit', as: :edit_user
   patch'users/:id', to: 'users#update'
   get  'users/:id', to: 'users#show', as: :user
+
+  get    '/users/:user_id/itineraries/:itinerary_id/activities', to: 'activities#index', as: :user_itinerary_activities
+  post   '/users/:user_id/itineraries/:itinerary_id/activities', to: 'activities#create'
+  get    '/users/:user_id/itineraries/:itinerary_id/activities/new', to: 'activities#new', as: :new_user_itinerary_activity
+  get    '/users/:user_id/itineraries/:itinerary_id/activities/:id/edit', to: 'activities#edit', as: :edit_user_itinerary_activity
+  get    '/users/:user_id/itineraries/:itinerary_id/activities/:id', to: 'activities#show', as: :user_itinerary_activity
+  patch  '/users/:user_id/itineraries/:itinerary_id/activities/:id', to: 'activities#update'
+  put    '/users/:user_id/itineraries/:itinerary_id/activities/:id', to: 'activities#update'
+  delete '/users/:user_id/itineraries/:itinerary_id/activities/:id', to: 'activities#destroy'
+
+  # user_itinerary routes
+  get    '/users/:id/itineraries', to: 'itineraries#index'  , as:   :user_itineraries
+  post   '/users/:id/itineraries', to:  'itineraries#create'
+  get    '/users/:id/itineraries/new', to: 'itineraries#new', as: :new_user_itinerary
+  get    '/users/:id/itineraries/:id/edit', to: 'itineraries#edit', as: :edit_user_itinerary
+  get    '/users/:id/itineraries/:id', to:'itineraries#show', as: :user_itinerary
+  patch  '/users/:id/itineraries/:id', to: 'itineraries#update'
+  put    '/users/:id/itineraries/:id', to:  'itineraries#update'
+  delete '/users/:id/itineraries/:id',  to: 'itineraries#destroy'
 
   # city routes
   get '/cities', to: 'cities#index', as: :cities
@@ -28,16 +49,6 @@ Rails.application.routes.draw do
  put    '/cities/:id/itineraries/:id', to:  'itineraries#update'
  delete '/cities/:id/itineraries/:id',  to: 'itineraries#destroy'
 
-# user_itinerary routes
-get    '/users/:id/itineraries', to: 'itineraries#index'  , as:   :user_itineraries
-post   '/users/:id/itineraries', to:  'itineraries#create'
-get    '/users/:id/itineraries/new', to: 'itineraries#new', as: :new_user_itinerary
-get    '/users/:id/itineraries/:id/edit', to: 'itineraries#edit', as: :edit_user_itinerary
-get    '/users/:id/itineraries/:id', to:'itineraries#show', as: :user_itinerary
-patch  '/users/:id/itineraries/:id', to: 'itineraries#update'
-put    '/users/:id/itineraries/:id', to:  'itineraries#update'
-delete '/users/:id/itineraries/:id',  to: 'itineraries#destroy'
-
 # sessions routes
 get "/login", to: "sessions#new", as: :login
 post "/sessions", to: "sessions#create"
@@ -45,15 +56,14 @@ get  "/logout", to: "sessions#destroy"
 
 end
 
-#
-#   resources :users do
-#     resources :itineraries do
-#       resources :activities
-#   end
-#  end
+# resources :users do
+#   resources :itineraries do
+#     resources :activities
+# end
+# end
 #
 #
 # resources :cities do
-#   resources :itineraries do
+# resources :itineraries do
 # end
 # end
