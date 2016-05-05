91842c1b2e24200678b4c1d6ae75adc34b96e56d
seanvsville <seanvanho@gmail.com>
2016-05-05 13:01:47 -0700

    added duration col to iten

--- db/migrate/20160505195443_add_duration_to_activities.rb
+++
@@ -1,5 +0,0 @@
-class AddDurationToActivities < ActiveRecord::Migration
-  def change
-    add_column :activities, :duration, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505191636) do
+ActiveRecord::Schema.define(version: 20160505195518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160505191636) do
     t.datetime "updated_at", null: false
     t.integer  "city_id"
     t.integer  "user_id"
+    t.integer  "duration"
   end
 
   add_index "itineraries", ["city_id"], name: "index_itineraries_on_city_id", using: :btree
