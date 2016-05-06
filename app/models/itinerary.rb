d36bf0513fd67c37ba7de3fae41b43169e9292f9
morgansharif <morgan.sharif@gmail.com>
2016-05-06 10:41:06 -0700

    resolved fk violations on delete

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,5 +1,10 @@
 class Itinerary < ActiveRecord::Base
   belongs_to :city
   belongs_to :user
-  has_many :activities, :dependent => :delete_all
+  has_many :activities
+  before_destroy :delete_activities
+
+  def delete_activities
+    self.activities.delete_all
+  end
 end
