86090ad3e5d1f77c8855361bc77defbc0432e7a1
seanvsville <seanvanho@gmail.com>
2016-05-06 12:30:36 -0700

    created itinerary_activities controller

---
+++ app/controllers/itinerary_activities_controller.rb
@@ -0,0 +1,20 @@
+class ItineraryActivitiesController < ApplicationController
+
+  def new
+    @activity = Activity.new
+    redirect_to
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/helpers/itinerary_activities_helper.rb
@@ -0,0 +1,2 @@
+module ItineraryActivitiesHelper
+end
