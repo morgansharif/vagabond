adeecca5355323fb72d072433235ce04396d99bb
seanvsville <seanvanho@gmail.com>
2016-05-06 12:42:17 -0700

    itin-act controller progress

--- app/controllers/itinerary_activities_controller.rb
+++ app/controllers/itinerary_activities_controller.rb
@@ -2,19 +2,37 @@ class ItineraryActivitiesController < ApplicationController
 
   def new
     @activity = Activity.new
-    redirect_to
+    @user = current_user
+    redirect_to itinerary_path(@user)
   end
 
   def create
+    @activity = Activity.new(activity_params)
+    @activity.itinerary_id = current_user.id
+    if @activity.save
+    redirect_to itinerary_path(@activity.itinerary_id)
+    else
+      redirect_to new_activity_path
+    end
   end
 
   def edit
+    @activity = Activity.find_by_id(params[:id])
+    render :edit
   end
 
   def update
+    @activity = Activity.find_by_id(params[:id])
+
   end
 
   def destroy
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name :desc :location :duration :img :itinerary_id)
   end
 
 end
