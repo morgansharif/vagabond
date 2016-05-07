36ecd39ddf350eee00715ad89ea3d57dc95452ce
morgansharif <morgan.sharif@gmail.com>
2016-05-06 20:32:21 -0700

    itinerary_activities#edit/update

--- app/controllers/itinerary_activities_controller.rb
+++ app/controllers/itinerary_activities_controller.rb
@@ -1,8 +1,8 @@
 class ItineraryActivitiesController < ApplicationController
 
   def new
+    @activity = Activity.new(activity_params)
     @itinerary = Itinerary.find_by_id(params[:itinerary_id])
-    @activity = Activity.new
     @activity.itinerary_id = @itinerary.id
   end
 
@@ -18,13 +18,18 @@ class ItineraryActivitiesController < ApplicationController
   end
 
   def edit
-    @activity = Activity.find_by_id(params[:itinerary_id])
-    render :edit
+    @activity = Activity.find_by_id(params[:activity_id])
+    @itinerary = Itinerary.find_by_id(params[:itinerary_id])
+    @activity.itinerary_id = @itinerary.id
   end
 
   def update
-    @activity = Activity.find_by_id(params[:id])
-
+    @activity = Activity.find_by_id(params[:activity_id])
+    if @activity.update(activity_params)
+      redirect_to itinerary_path(params[:itinerary_id])
+    else
+      redirect_to new_itinerary_activity(params[:itinerary_id])
+    end
   end
 
   def destroy
