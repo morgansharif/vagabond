d7fce89b308f51617577b02c80469532bb72836b
lamchristine <christine.lam.06@gmail.com>
2016-05-08 13:57:35 -0700

    profile image restricted to specified size

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'bcrypt', '~> 3.1.7'
 #used to upload photos
 gem 'carrierwave', '~> 0.11.2'
 
+#image processing
+gem 'mini_magick', '~> 4.5', '>= 4.5.1'
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -30,6 +30,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+
+  process resize_to_fill: [200, 200]
 
   # Create different versions of your uploaded files:
   # version :thumb do
