272811123e9ab51854820a313dbc44bb4aabd497
Ameer Brown <akeithbrown@gmail.com>
2016-05-08 10:52:52 -0700

    create welcome_user method in UserMailer mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,6 @@
 class UserMailer < ApplicationMailer
+  def welcome_user (user)
+  @user = user
+  mail to: [@user.email], subject: "Welcome to the Vagabon community, #{@user.first_name}"
+  end
 end
