917c05d5419c3cc47ebf3efaa4f8ef70ad0246f0
lamchristine <christine.lam.06@gmail.com>
2016-05-08 21:58:48 -0700

    added production code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'murmuring-brook-27298.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
