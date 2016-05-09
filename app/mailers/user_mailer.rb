32b7c9466f9535601a6fb70e71d1399f6ac96b28
Ameer Brown <akeithbrown@gmail.com>
2016-05-09 02:36:53 -0700

    styled registration_confirmation.html.erb, next delete inline css  and get scss file to work

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
   default :from => "vagabond@scam.com"
 
   def registration_confirmation(user)
-    attachments.inline['travel.gif'] = File.read('#{Rails.root}/app/assets/images/travel.gif')
+    attachments.inline["travel.gif"] = File.read("#{Rails.root}/app/assets/images/travel.gif")
     @user = user
     mail(:to => "#{user.first_name} <#{user.email}>", :subject =>"Welcome to the Vagabond community, #{user.first_name}")
   end
